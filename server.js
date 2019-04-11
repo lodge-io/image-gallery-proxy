84b6f5b86339d0100248170d9b2ca5ab67fb772e
Michael Frenkel <michael.frenkel@outlook.com>
2019-04-11 15:13:04 -0700

    render image gallery with images displaying on proxy server

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const httpProxy = require('http-proxy');
-// const proxy = require('http-proxy-middleware');
 const path = require('path');
+const cors = require('cors');
+const axios = require('axios');
+// const proxy = require('http-proxy-middleware');
 const app = express();
 const port = 8001;
-app.use('/', express.static('dist'));
 
+app.use(express.static(path.join(__dirname, 'dist')))
+app.use('/rooms/:id', express.static(path.join(__dirname, '../dist')));
+
+const imageGallery = axios.create({
+  baseURL: 'http://localhost:8000'
+});
+
+const description = axios.create({
+  baseURL: 'http://localhost:3116'
+});
+
+app.get('/rooms/:id/', (req, res) => {
+  imageGallery.get(`/rooms/${req.params.id}`)
+    .then(response => res.json(response.data))
+    .catch(err => res.send(err))
+});
+   
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
 });
